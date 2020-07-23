ce6397bf1394edbd9c9e42b4b4d80d430eaaac9b
davidcorne <davidcorne@gmail.com>
2020-07-23 10:57:38 +0100

    Start playing with bot music

--- index.js
+++ index.js
@@ -12,3 +12,30 @@ bot.login(TOKEN)
 bot.on('ready', function () {
   console.log(`Logged in as ${bot.user.tag}!`)
 })
+
+const join = async function (message) {
+  if (message.member.voice.channel) {
+    const connection = await message.member.voice.channel.join()
+    const dispatcher = connection.play('SoundHelix-Song-1.mp3')
+    dispatcher.on('start', () => {
+      console.log('Started playing')
+    })
+    dispatcher.on('finish', () => {
+      console.log('Finished playing')
+    })
+    dispatcher.on('error', console.error)
+  }
+}
+
+const commands = {
+  '!join': join
+}
+
+bot.on('message', async message => {
+  for (const command in commands) {
+    if (message.content.startsWith(command)) {
+      console.debug(`Command ${command} parsed`)
+      commands[command](message)
+    }
+  }
+})
